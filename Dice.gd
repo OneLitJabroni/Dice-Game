c411be8583c8a1b7d25588aa9a9b5da202d4df56
Nate <Piercey1995@gmail.com>
2024-12-05 20:51:19 -0500

    stable base game version

--- Dice.gd
+++ Dice.gd
@@ -3,28 +3,20 @@ extends RigidBody3D
 
 @onready var raycasts = $Raycasts.get_children()
 var start_pos
-var roll_strength = 30
+var roll_strength = 175
+
 
 
 signal roll_finished(value)
 
 
-func _ready():
-	start_pos = global_position
-# Add dice faces to the ItemList
 
 
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		_roll()
-
-
-func _roll():
+func roll():
 	#reset state
 	sleeping = false
 	freeze = false
-	transform.origin = start_pos
 	linear_velocity = Vector3.ZERO
 	angular_velocity = Vector3.ZERO
 	
@@ -33,11 +25,14 @@ func _roll():
 	transform.basis = Basis(Vector3.UP, randf_range(0, 2 * PI)) * transform.basis
 	transform.basis = Basis(Vector3.FORWARD, randf_range(0, 2 * PI)) * transform.basis
 	
-	
 	#random throw impulse
 	var throw_vector = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
 	angular_velocity = throw_vector * roll_strength / 2
 	apply_central_impulse(throw_vector * roll_strength)
+	
+
+	
+
 
 	
 
@@ -47,6 +42,9 @@ func _on_sleeping_state_changed():
 		for raycast in raycasts:
 			if raycast.is_colliding():
 				roll_finished.emit(raycast.opposite_side)
-		
-		
-		
+			
+			
+# Update this method to accept the value
+func _on_roll_finished(value):
+	print("Roll finished for", self.name, "with value:", value)
+	# Here, you can process the value (e.g., update the UI, show the result, etc.)
--- World.gd
+++ World.gd
@@ -1,15 +1,39 @@
 extends Node3D
 
 
+@onready var dice1 = $Dice1
+@onready var dice2 = $Dice2
+@onready var dice3 = $Dice3
+@onready var dice4 = $Dice4
+@onready var dice5 = $Dice5
+
+
 @onready var texture_rect_1 = $CanvasLayer/TextureRect1
-
 @onready var texture_rect_2 = $CanvasLayer/TextureRect2
-
 @onready var texture_rect_3 = $CanvasLayer/TextureRect3
-
 @onready var texture_rect_4 = $CanvasLayer/TextureRect4
-
 @onready var texture_rect_5 = $CanvasLayer/TextureRect5
+
+
+@onready var result_label1 = $CanvasLayer/Label1
+@onready var result_label2 = $CanvasLayer/Label2
+@onready var result_label3 = $CanvasLayer/Label3
+@onready var result_label4 = $CanvasLayer/Label4
+@onready var result_label5 = $CanvasLayer/Label5
+
+
+
+
+var dice_array = [dice1,dice2,dice3,dice4,dice5]
+var selected_dice =  []
+var first_roll = true  # Flag to check if it's the first roll
+var rolls_left = 3  # Player starts with 3 rolls
+
+
+
+
+
+
 
 
 var dice_images = {
@@ -24,25 +48,78 @@ var dice_images = {
 
 
 
+func _ready():
+	texture_rect_1.connect("gui_input", Callable(self, "_on_texture_rect_input").bind(dice1, texture_rect_1))
+	texture_rect_2.connect("gui_input", Callable(self, "_on_texture_rect_input").bind(dice2, texture_rect_2))
+	texture_rect_3.connect("gui_input", Callable(self, "_on_texture_rect_input").bind(dice3, texture_rect_3))
+	texture_rect_4.connect("gui_input", Callable(self, "_on_texture_rect_input").bind(dice4, texture_rect_4))
+	texture_rect_5.connect("gui_input", Callable(self, "_on_texture_rect_input").bind(dice5, texture_rect_5))
+
+	
+	
+	# Handle click on TextureRect and select corresponding dice
+func _on_texture_rect_input(event: InputEvent, dice_instance: RigidBody3D, texture_rect: TextureRect):
+	if event is InputEventMouseButton and event.pressed:
+  # Retrieve the ColorRect child
+		if selected_dice.has(dice_instance):
+			texture_rect.modulate = Color(1, 1, 1)
+			selected_dice.erase(dice_instance)
+			print(dice_instance.name, "deselected")
+		else:
+			texture_rect.modulate = Color(1, 0.8, 0)  # Highlight the dice
+			selected_dice.append(dice_instance)       # Add to selected dice
+			print(dice_instance.name, "selected")
+		# Highlight the selected TextureRect (optional visual feedback)
 
 
 
+# The function that handles the input event to trigger the roll
+func _input(event):
+	if event.is_action_pressed("ui_accept"):  # Trigger the roll action (e.g., Enter or Space)
+		if rolls_left > 0:  # Check if the player still has rolls left
+			if first_roll:
+				roll_all_dice()  # If it's the first roll, roll all dice
+				first_roll = false  # Set the flag to false after the first roll
+			else:
+				roll_selected_dice()  # Roll only selected dice
+			rolls_left -= 1  # Decrement rolls left after each roll
+			print("Rolls left: ", rolls_left)
+		else:
+			print("No more rolls left!")  # If no rolls are left, show a message
 
+# Function to roll all dice (for the first roll)
+func roll_all_dice():
+	for dice in dice_array:
+		get_tree().call_group("dice_group", "roll")
+
+
+
+# Function to roll only the selected dice
+func roll_selected_dice():
+	for dice in selected_dice:
+		if dice.has_method("roll"):
+			dice.roll()
+
+		
+
+# Update the visual feedback for selected dice (e.g., change color to yellow when selected)
 func _on_dice_1_roll_finished(value):
 	texture_rect_1.texture = dice_images.get(value)
+	result_label1.text = str(1)
 
 
 func _on_dice_2_roll_finished(value):
 	texture_rect_2.texture = dice_images.get(value)
-
+	result_label2.text = str(2)
 
 func _on_dice_3_roll_finished(value):
 	texture_rect_3.texture = dice_images.get(value)
-
+	result_label3.text = str(3)
 
 func _on_dice_4_roll_finished(value):
 	texture_rect_4.texture = dice_images.get(value)
-
+	result_label4.text = str(4)
 
 func _on_dice_5_roll_finished(value):
 	texture_rect_5.texture = dice_images.get(value)
+	result_label5.text = str(5)
